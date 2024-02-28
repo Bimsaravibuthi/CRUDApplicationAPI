cca5ee625109659683bce086f22a1210cd5fc3f4
Bimsaravibuthi <bimsaravibuthi1541@gmail.com>
2024-02-28 16:35:03 +0530

    Input parameter lenght validated by annotations.

--- CRUDApplicationAPI.Models/ManageDepartmentModel.cs
+++ CRUDApplicationAPI.Models/ManageDepartmentModel.cs
@@ -5,8 +5,10 @@ namespace CRUDApplicationAPI.Models
     public class ManageDepartmentModel
     {
         [Required(ErrorMessage ="Code is required")]
+        [MaxLength(2, ErrorMessage ="Allowed only 2 characters")]
         public string? Code { get; set; }
         [Required(ErrorMessage = "Name is required")]
+        [MaxLength(30, ErrorMessage = "Allowed only 30 characters")]
         public string? Name { get; set; }
     }
 }
--- CRUDApplicationAPI.Models/ManageEmployeeModel.cs
+++ CRUDApplicationAPI.Models/ManageEmployeeModel.cs
@@ -5,11 +5,14 @@ namespace CRUDApplicationAPI.Models
     public class ManageEmployeeModel
     {
         [Required(ErrorMessage ="Firstname is required")]
+        [MaxLength(20, ErrorMessage = "Allowed only 20 characters")]
         public string? Firstname { get; set; }
         [Required(ErrorMessage = "Lastname is required")]
+        [MaxLength(20, ErrorMessage = "Allowed only 20 characters")]
         public string? Lastname { get; set;}
         [Required(ErrorMessage = "Email address is required")]
         [EmailAddress(ErrorMessage ="Email address is not valid")]
+        [MaxLength(30, ErrorMessage = "Allowed only 30 characters")]
         public string? Email { get; set; }
         [Required(ErrorMessage ="Date of birth is required")]
         public DateTime Dateofbirth { get; set; }
